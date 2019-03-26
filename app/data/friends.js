20ff679f4e2cac13b628ec0185fb9155cfedcfb5
Mario Franco <MFranc0@outlook.com>
2019-03-26 16:45:03 -0700

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var friendsData = [
+    {
+        name: "Gregor Clegane",
+        photo: "https://images-cdn.9gag.com/photo/a5nXD1N_700b.jpg",
+        scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+        name: "Jon Snow",
+        photo: "https://ksassets.timeincuk.net/wp/uploads/sites/55/2016/05/250116GOTJonSnow_SYK_250116-920x610.jpg",
+        scores: [2, 2, 3, 2, 2, 2, 5, 2, 2, 2]
+    },
+    {
+        name: "Cersei Lannister",
+        photo: "https://nationalpostcom.files.wordpress.com/2017/09/cerseiclinton1.jpg?quality=80&strip=all&w=780",
+        scores: [1, 1, 1, 4, 1, 1, 1, 3, 1, 1]
+    },
+    {
+        name: "Tyrion Lannister",
+        photo: "https://s3.amazonaws.com/sr-application/images/pingpongrecruits/2018/5/146_269298_tyrion-lannister-eastwatch.jpg",
+        scores: [3, 2, 3, 3, 3, 1, 3, 3, 4, 3]
+    },
+    {
+        name: "Joffrey Baratheon",
+        photo: "https://i.redd.it/bd0dy9s62ua21.jpg",
+        scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+        name: "Khal Drogo",
+        photo: "https://imagesvc.timeincapp.com/v3/fan/image?url=https%3A%2F%2Fwinteriscoming.net%2Ffiles%2F2017%2F05%2Frehost2F20162F92F142Feb52b269-15bc-40f5-a22a-405dca3c45fd.jpg&w=736&h=485&c=sc",
+        scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+        name: "Night King",
+        photo: "https://awoiaf.westeros.org/thumb.php?f=Night_king.jpg&width=300",
+        scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    },
+    {
+        name: "Margaery Tyrell",
+        photo: "http://images6.fanpop.com/image/photos/39700000/Margaery-Tyrell-Season-6-EW-Promotional-Photoshoot-margaery-tyrell-39724602-1280-1706.jpg",
+        scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+];
+// Export to API Route
+module.exports = friendsData;
